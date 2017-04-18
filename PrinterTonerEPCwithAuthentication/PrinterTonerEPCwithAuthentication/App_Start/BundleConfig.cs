ec4ade289a88e2d512a39798141bb57fa73c28ca
npejic <npejic@gmail.com>
2017-04-18 22:50:04 +0200

    Added DatePicker Calendar (to ContactCreateView)

--- PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/App_Start/BundleConfig.cs
+++ PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/App_Start/BundleConfig.cs
@@ -26,6 +26,14 @@ namespace PrinterTonerEPCwithAuthentication
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //Create bundle for jQueryUI  
+            //js  
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                      "~/Scripts/jquery-ui-{version}.js"));
+            //css  
+            bundles.Add(new StyleBundle("~/Content/cssjqryUi").Include(
+                   "~/Content/jquery-ui.css"));  
         }
     }
 }
