a7e8845f254165c3f1c5494bd3015c06fec75c80
npejic <npejic@gmail.com>
2017-04-18 21:31:10 +0200

    Owners Controller and Views completed

---
+++ PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/Controllers/OwnersController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PrinterTonerEPCwithAuthentication.Models;
+
+namespace PrinterTonerEPCwithAuthentication.Controllers
+{
+    public class OwnersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index(string searchByOwner)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var owners = from o in db.Owners
+                         orderby o.OwnerName
+                         select o;
+
+            if (!String.IsNullOrEmpty(searchByOwner))
+            {
+                owners = owners.Where(o => o.OwnerName.Contains(searchByOwner)).OrderBy(o=>o.OwnerName);
+            }
+            return View(owners.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Owner owner = db.Owners.Find(id);
+            if (owner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(owner);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OwnerID,OwnerName,OwnerTelephone,OwnerAddress,OwnerContact,OwnerContactTelephone,OwnerPIB,OwnerMatBroj,OwnerIsInPDV, OwnerEmail,OwnerIsActive,Created, Napomena")] Owner owner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Owners.Add(owner);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(owner);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Owner owner = db.Owners.Find(id);
+            if (owner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(owner);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OwnerID,OwnerName,OwnerTelephone,OwnerAddress,OwnerContact,OwnerContactTelephone,OwnerPIB,OwnerMatBroj,OwnerIsInPDV, OwnerEmail,OwnerIsActive,Created, Napomena")] Owner owner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(owner).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(owner);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Owner owner = db.Owners.Find(id);
+            if (owner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(owner);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Owner owner = db.Owners.Find(id);
+            db.Owners.Remove(owner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
