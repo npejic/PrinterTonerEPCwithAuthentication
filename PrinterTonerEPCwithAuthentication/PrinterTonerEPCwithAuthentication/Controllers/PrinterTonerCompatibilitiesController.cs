a02a4d114d46cb5f169796165f29e64ce7e24dcb
npejic <npejic@gmail.com>
2017-04-19 09:05:08 +0200

    Added PrinterTonerCompatibility and Sales Controllers and Views

---
+++ PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/Controllers/PrinterTonerCompatibilitiesController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PrinterTonerEPCwithAuthentication.Models;
+
+namespace PrinterTonerEPCwithAuthentication.Controllers
+{
+    public class PrinterTonerCompatibilitiesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: PrinterTonerCompatibilities
+        public ActionResult Index(string searchByPrinter, string searchByToner)
+        {
+            var printerTonerCompatibilitys = db.PrinterTonerCompatibilitys.Include(p => p.Printer).Include(p => p.Toner).OrderBy(p=>p.Printer.PrinterManufacturer).ThenBy(p=>p.Printer.PrinterModel);
+
+            if (!String.IsNullOrEmpty(searchByPrinter))
+            {
+                printerTonerCompatibilitys = printerTonerCompatibilitys.Where(o => o.Printer.PrinterModel.Contains(searchByPrinter)).OrderBy(p => p.Printer.PrinterManufacturer).ThenBy(p => p.Printer.PrinterModel);
+            }
+
+
+            if (!String.IsNullOrEmpty(searchByToner))
+            {
+                printerTonerCompatibilitys = printerTonerCompatibilitys.Where(o => o.Toner.TonerModel.Contains(searchByToner)).OrderBy(p => p.Printer.PrinterManufacturer).ThenBy(p => p.Printer.PrinterModel);
+            }
+
+            return View(printerTonerCompatibilitys.ToList());
+        }
+
+        // GET: PrinterTonerCompatibilities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrinterTonerCompatibility printerTonerCompatibility = db.PrinterTonerCompatibilitys.Find(id);
+            if (printerTonerCompatibility == null)
+            {
+                return HttpNotFound();
+            }
+            return View(printerTonerCompatibility);
+        }
+
+        // GET: PrinterTonerCompatibilities/Create
+        public ActionResult Create()
+        {
+            //da se ne bi pojavljivali duplikati iz PrinterModela
+            var printerModelNoDuplicate = db.Printers.GroupBy(s => s.PrinterModel).Select(x => x.FirstOrDefault());
+            ViewBag.PrinterID = new SelectList(printerModelNoDuplicate, "PrinterID", "PrinterModel");
+
+            ViewBag.TonerID = new SelectList(db.Toners, "TonerID", "TonerModel");
+            return View();
+        }
+
+        // POST: PrinterTonerCompatibilities/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PrinterTonerCompatibilityID,PrinterID,TonerID")] PrinterTonerCompatibility printerTonerCompatibility)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PrinterTonerCompatibilitys.Add(printerTonerCompatibility);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterModel", printerTonerCompatibility.PrinterID);
+            ViewBag.TonerID = new SelectList(db.Toners, "TonerID", "TonerModel", printerTonerCompatibility.TonerID);
+            return View(printerTonerCompatibility);
+        }
+
+        // GET: PrinterTonerCompatibilities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrinterTonerCompatibility printerTonerCompatibility = db.PrinterTonerCompatibilitys.Find(id);
+            if (printerTonerCompatibility == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterModel", printerTonerCompatibility.PrinterID);
+            ViewBag.TonerID = new SelectList(db.Toners, "TonerID", "TonerModel", printerTonerCompatibility.TonerID);
+            return View(printerTonerCompatibility);
+        }
+
+        // POST: PrinterTonerCompatibilities/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PrinterTonerCompatibilityID,PrinterID,TonerID")] PrinterTonerCompatibility printerTonerCompatibility)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(printerTonerCompatibility).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterModel", printerTonerCompatibility.PrinterID);
+            ViewBag.TonerID = new SelectList(db.Toners, "TonerID", "TonerModel", printerTonerCompatibility.TonerID);
+            return View(printerTonerCompatibility);
+        }
+
+        // GET: PrinterTonerCompatibilities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrinterTonerCompatibility printerTonerCompatibility = db.PrinterTonerCompatibilitys.Find(id);
+            if (printerTonerCompatibility == null)
+            {
+                return HttpNotFound();
+            }
+            return View(printerTonerCompatibility);
+        }
+
+        // POST: PrinterTonerCompatibilities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PrinterTonerCompatibility printerTonerCompatibility = db.PrinterTonerCompatibilitys.Find(id);
+            db.PrinterTonerCompatibilitys.Remove(printerTonerCompatibility);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/Controllers/SalesController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PrinterTonerEPCwithAuthentication.Models;
+
+namespace PrinterTonerEPCwithAuthentication.Controllers
+{
+    public class SalesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            var sales = db.Sales.Include(s => s.Contract).Include(s => s.Printer).OrderBy(s => s.Contract.ContractName).ThenBy(s => s.Contract.ContractDate);
+            return View(sales.ToList());
+        }
+
+        public ActionResult SalesReportByOwner(string searchByOwner)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var sales = db.Sales.Where(s=>s.Printer.Owner.OwnerName=="EPC DOO").OrderBy(s => s.Contract.Owner.OwnerName).ThenBy(s => s.Contract.ContractName);
+
+            if (!string.IsNullOrEmpty(searchByOwner))
+            {
+                sales = sales.Where(s => s.Contract.Owner.OwnerName.Contains(searchByOwner)).OrderBy(s => s.Contract.Owner.OwnerName).ThenBy(s => s.Contract.ContractName);// && s.printer.isepcprinter==true);
+            }
+
+            return View(sales.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sales.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        public ActionResult CreateByOwner()
+        {
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterSerialNo");
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            var printersNotOwnedByEPC = db.Printers.Where(p => p.Owner.OwnerName == "EPC DOO");
+            ViewBag.ContractID = new SelectList(db.Contracts, "ContractID", "ContractName");
+            ViewBag.PrinterID = new SelectList(printersNotOwnedByEPC, "PrinterID", "PrinterSerialNo");
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SaleID,SaleDate,Price,LocationOfPrinterIs,ContractID,AlternateContract,PrinterID,TonerID,Created")] Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sales.Add(sale);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ContractID = new SelectList(db.Contracts, "ContractID", "ContractName", sale.ContractID);
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterInternalNo", sale.PrinterID);
+            return View(sale);
+        }
+
+        public ActionResult EditSalesReportByOwner(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sales.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ContractID = new SelectList(db.Contracts, "ContractID", "ContractName", sale.ContractID);
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterInternalNo", sale.PrinterID);
+            
+            return View(sale);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditSalesReportByOwner([Bind(Include = "SaleID,SaleDate,Price,LocationOfPrinterIs,ContractID,AlternateContract,PrinterID,TonerID,Created")] Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sale).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("SalesReportByOwner");
+            }
+            ViewBag.ContractID = new SelectList(db.Contracts, "ContractID", "ContractName", sale.ContractID);
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterInternalNo", sale.PrinterID);
+           
+            return View(sale);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sales.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ContractID = new SelectList(db.Contracts, "ContractID", "ContractName", sale.ContractID);
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterInternalNo", sale.PrinterID);
+            
+            return View(sale);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SaleID,SaleDate,Price,LocationOfPrinterIs,ContractID,AlternateContract,PrinterID,TonerID,Created")] Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sale).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ContractID = new SelectList(db.Contracts, "ContractID", "ContractName", sale.ContractID);
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterInternalNo", sale.PrinterID);
+            
+            return View(sale);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sales.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sale sale = db.Sales.Find(id);
+            db.Sales.Remove(sale);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
