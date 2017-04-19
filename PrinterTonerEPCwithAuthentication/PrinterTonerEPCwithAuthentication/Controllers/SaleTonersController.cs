e9114d58e9b5ae6ce1bef6892b567a47f01d4c9a
npejic <npejic@gmail.com>
2017-04-19 10:07:08 +0200

    Added Sale and Servis Controllers and Views

---
+++ PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/Controllers/SaleTonersController.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PrinterTonerEPCwithAuthentication.Models;
+
+namespace PrinterTonerEPCwithAuthentication.Controllers
+
+{
+    public class SaleTonersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            //var saleToners = db.SaleToners.Include(s => s.Owner).Include(s => s.Toner)
+            //                    .OrderBy(s => s.Owner.OwnerName).ThenBy(s => s.Toner.TonerModel).ThenBy(s => s.SaleTonerDate);
+
+            var saleToners = db.SaleToners.Include(s => s.Owner).Include(s => s.Toner).OrderByDescending(s => s.SaleTonerDate).ThenBy(c=>c.Owner.OwnerName);
+
+            return View(saleToners.ToList());
+        }
+
+        //Returns list of owners (companies) that didn't order toners in last X (periodInMonths) months
+        //Report No.5
+        public ActionResult TonerAlarm(string periodInMonths)
+        {
+
+            var ownersWithNoAlarmOrder = db.SaleToners.GroupBy(g => new { g.Owner.OwnerName, g.TonerID }).Select(s => s.OrderByDescending(x => x.SaleTonerDate)
+                                        .FirstOrDefault()).OrderBy(s => s.Owner.OwnerName).ThenBy(s => s.Toner.TonerModel);
+
+            if (!String.IsNullOrEmpty(periodInMonths))
+            {
+                int period = Int16.Parse(periodInMonths);
+                var LimitDate = DateTime.Now.Date;
+                LimitDate = LimitDate.AddMonths(-period);
+                ownersWithNoAlarmOrder = ownersWithNoAlarmOrder.Where(o => o.SaleTonerDate < LimitDate && o.Owner.OwnerIsActive == true).OrderBy(s => s.Owner.OwnerName).ThenBy(s => s.Toner.TonerModel).ThenBy(s => s.SaleTonerDate);
+            }
+            return View(ownersWithNoAlarmOrder.ToList());
+        }
+
+        //Report No.6
+        public ActionResult TotalTonerSale(string dateFromString, string dateToString)
+        {
+            var soldToners = db.SaleToners.GroupBy(r => r.Toner.TonerModel).Select(r => new TonerTotal()
+            {
+                TotalTonerModel = r.Key,
+                TonerTotalCount = r.Sum(c=> c.TonerQuantity),
+            }).OrderByDescending(c => c.TonerTotalCount).ToList();
+            
+            //TODO: missing part of the code which will do TryParse DateTime input
+            if (!String.IsNullOrEmpty(dateFromString) || !String.IsNullOrEmpty(dateToString))
+            {
+                DateTime dateFrom = Convert.ToDateTime(dateFromString);
+                //if (DateTime.TryParse(dateFromString, out dateFrom))
+                //{
+
+                //}
+                DateTime dateTo = Convert.ToDateTime(dateToString);
+                var soldTonersInPeriod = db.SaleToners.Where(c => c.SaleTonerDate >= dateFrom && c.SaleTonerDate <= dateTo).GroupBy(r => r.Toner.TonerModel).Select(r => new TonerTotal()
+                {
+                    TotalTonerModel = r.Key,
+                    TonerTotalCount = r.Sum(c => c.TonerQuantity),
+                }).OrderByDescending(c => c.TonerTotalCount).ToList();
+
+                soldToners = soldTonersInPeriod;
+            }
+            
+            //Izračunava ukupan broj EPC štampača na iznajmljivanju
+            var CountSoldToners = soldToners.Sum(c=>c.TonerTotalCount);
+            ViewData["CountSoldToners"] = CountSoldToners;
+
+            return View(soldToners.ToList());
+        }
+
+        //Report No.4
+        public ActionResult LastTonerSale(string searchByOwner, string searchByToner)
+        {
+            var lastTonerSale = db.SaleToners.GroupBy(g => new { g.Owner.OwnerName, g.TonerID }).Select(s => s.OrderByDescending(x => x.SaleTonerDate).FirstOrDefault()).OrderBy(s => s.Owner.OwnerName).ThenBy(s => s.Toner.TonerModel);
+
+            if (!String.IsNullOrEmpty(searchByOwner))
+            {
+                lastTonerSale = lastTonerSale.Where(o => o.Owner.OwnerName.Contains(searchByOwner)).OrderBy(s => s.Owner.OwnerName).ThenBy(s => s.Toner.TonerModel).ThenBy(s => s.SaleTonerDate);
+            }
+
+            if (!String.IsNullOrEmpty(searchByToner))
+            {
+                lastTonerSale = lastTonerSale.Where(o => o.Toner.TonerModel.Contains(searchByToner)).OrderBy(s => s.Owner.OwnerName).ThenBy(s => s.Toner.TonerModel).ThenBy(s => s.SaleTonerDate);
+            }
+
+            return View(lastTonerSale.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SaleToner saleToner = db.SaleToners.Find(id);
+            if (saleToner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(saleToner);
+        }
+
+        public ActionResult Create()
+        {
+            var orderedOwners = db.Owners.OrderBy(c => c.OwnerName);
+            ViewBag.OwnerID = new SelectList(orderedOwners, "OwnerID", "OwnerName");
+
+            var orderedToners = db.Toners.OrderBy(c => c.TonerModel);
+            ViewBag.TonerID = new SelectList(orderedToners, "TonerID", "TonerModel");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SaleTonerID,SaleTonerDate,TonerPrice,OwnerID,TonerID,TonerQuantity,InvoiceNo")] SaleToner saleToner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SaleToners.Add(saleToner);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.OwnerID = new SelectList(db.Owners, "OwnerID", "OwnerName", saleToner.OwnerID);
+            ViewBag.TonerID = new SelectList(db.Toners, "TonerID", "TonerModel", saleToner.TonerID);
+            return View(saleToner);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SaleToner saleToner = db.SaleToners.Find(id);
+            if (saleToner == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OwnerID = new SelectList(db.Owners, "OwnerID", "OwnerName", saleToner.OwnerID);
+            ViewBag.TonerID = new SelectList(db.Toners, "TonerID", "TonerModel", saleToner.TonerID);
+            return View(saleToner);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SaleTonerID,SaleTonerDate,TonerPrice,OwnerID,TonerID,TonerQuantity,InvoiceNo")] SaleToner saleToner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(saleToner).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OwnerID = new SelectList(db.Owners, "OwnerID", "OwnerName", saleToner.OwnerID);
+            ViewBag.TonerID = new SelectList(db.Toners, "TonerID", "TonerModel", saleToner.TonerID);
+            return View(saleToner);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SaleToner saleToner = db.SaleToners.Find(id);
+            if (saleToner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(saleToner);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SaleToner saleToner = db.SaleToners.Find(id);
+            db.SaleToners.Remove(saleToner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/Controllers/ServisController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PrinterTonerEPCwithAuthentication.Models;
+
+namespace PrinterTonerEPCwithAuthentication.Controllers
+{
+    public class ServisController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        int selectedOwnerForServis;
+
+        public ActionResult Index(string searchBySerialNo, string searchByOwner)
+        {
+            var servis = db.Servis.Include(s => s.Printer).OrderBy(s => s.Printer.Owner.OwnerName).ThenBy(o => o.ServisDate);
+
+            if (!String.IsNullOrEmpty(searchBySerialNo))
+            {
+                servis = servis.Where(o => o.Printer.PrinterSerialNo.Contains(searchBySerialNo)).OrderBy(s => s.Printer.Owner.OwnerName).ThenBy(o => o.ServisDate);
+            }
+
+            if (!String.IsNullOrEmpty(searchByOwner))
+            {
+                servis = servis.Where(o => o.Printer.Owner.OwnerName.Contains(searchByOwner)).OrderBy(s => s.Printer.Owner.OwnerName).ThenBy(o => o.ServisDate);
+            }
+
+            return View(servis.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Servis servis = db.Servis.Find(id);
+            if (servis == null)
+            {
+                return HttpNotFound();
+            }
+            return View(servis);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.OwnerID = new SelectList(db.Owners, "OwnerID", "OwnerName");
+            
+            var chosenOwner = (int)TempData["OwnerID"];
+            var printersBySelectedOwner = db.Printers.Where(p => p.OwnerID == chosenOwner);
+            
+            ViewBag.PrinterID = new SelectList(printersBySelectedOwner, "PrinterID", "PrinterSerialNo");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ServisID,ServisDate,ServisPrice,PrinterID,Remark,Created")] Servis servis)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Servis.Add(servis);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterSerialNo", servis.PrinterID);
+            return View(servis);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Servis servis = db.Servis.Find(id);
+            if (servis == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterSerialNo", servis.PrinterID);
+            return View(servis);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ServisID,ServisDate,ServisPrice,PrinterID,Remark,Created")] Servis servis)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(servis).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PrinterID = new SelectList(db.Printers, "PrinterID", "PrinterSerialNo", servis.PrinterID);
+            return View(servis);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Servis servis = db.Servis.Find(id);
+            if (servis == null)
+            {
+                return HttpNotFound();
+            }
+            return View(servis);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Servis servis = db.Servis.Find(id);
+            db.Servis.Remove(servis);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SelectOwner()
+        {
+            ViewBag.OwnerID = new SelectList(db.Owners, "OwnerID", "OwnerName");
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SelectOwner(Owner model)
+        {
+            selectedOwnerForServis = model.OwnerID;
+            TempData["OwnerID"] = model.OwnerID;
+            return RedirectToAction("Create");
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
