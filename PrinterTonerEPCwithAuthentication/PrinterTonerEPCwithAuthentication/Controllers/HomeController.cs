e57f98c0544c3dbc3f82bd8fc8ccfe4d8b3075cd
npejic <npejic@gmail.com>
2017-04-25 13:28:40 +0200

    Added ToDo list and links to 6 reports to HomeIndex View and Controller

--- PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/Controllers/HomeController.cs
+++ PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrinterTonerEPCwithAuthentication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,18 @@ namespace PrinterTonerEPCwithAuthentication.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            //Izračunava ukupan broj EPC štampača na iznajmljivanju
+            ApplicationDbContext db = new ApplicationDbContext();
+            var sales = from s in db.Sales
+                        where s.Printer.Owner.OwnerName == "EPC DOO"
+                        select s;
+            var CountRentedPrinters = sales.Count();
+            ViewData["CountRentedPrinters"] = CountRentedPrinters;
+
+            //list of opened todoes (without closing date), shown on top of the HomeIndexView
+            //var openedTasks = db.ToDoes.Include(t => t.ApplicationUser).Where(c => c.Closed == null).OrderBy(c => c.Created).ToList();
+            var openedTasks = db.ToDoes.Where(c => c.Closed == null).OrderBy(c => c.Created).ToList();
+            return View(openedTasks);
         }
 
         public ActionResult About()
