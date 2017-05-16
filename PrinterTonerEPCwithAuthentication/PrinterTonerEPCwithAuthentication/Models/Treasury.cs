b39d4dd3f8484e9dafe6747af3e9ca66fb3fe7ac
npejic <npejic@gmail.com>
2017-05-16 13:20:41 +0200

    Added Treasury model

---
+++ PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/Models/Treasury.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrinterTonerEPCwithAuthentication.Models
+{
+    public class Treasury : BaseClass
+    {
+        public Treasury()
+        {
+            this.ApplicationUser.Nick = HttpContext.Current.User.Identity.Name;
+        }
+        public int TreasuryID { get; set; }
+        public string ApplicationUserID { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+        public double Amount { get; set; }
+        public enum TypeOfExpence { putarina, reprezentacija, ostalo}
+        public TypeOfExpence Expence { get; set; }
+        public string Remark { get; set; }
+    }
+}
