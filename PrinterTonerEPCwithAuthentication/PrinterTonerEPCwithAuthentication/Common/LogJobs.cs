d937a7196a8895c343803345a94fbeb3ce9c872c
npejic <npejic@gmail.com>
2017-07-11 16:07:58 +0200

    Added LogJobs.cs

---
+++ PrinterTonerEPCwithAuthentication/PrinterTonerEPCwithAuthentication/Common/LogJobs.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace PrinterTonerEPCwithAuthentication.Common
+{
+    public class LogJobs
+    {
+        private void LogError(Exception ex)
+        {
+            string message = string.Format("Time: {0}", DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt"));
+            message += Environment.NewLine;
+            message += "-----------------------------------------------------------";
+            message += Environment.NewLine;
+            message += string.Format("Message: {0}", ex.Message);
+            message += Environment.NewLine;
+            message += string.Format("StackTrace: {0}", ex.StackTrace);
+            message += Environment.NewLine;
+            message += string.Format("Source: {0}", ex.Source);
+            message += Environment.NewLine;
+            message += string.Format("TargetSite: {0}", ex.TargetSite.ToString());
+            message += Environment.NewLine;
+            message += "-----------------------------------------------------------";
+            message += Environment.NewLine;
+            string path = System.Web.HttpContext.Current.Server.MapPath("~/ErrorLog/ErrorLog.txt");
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(message);
+                writer.Close();
+            }
+        }
+
+        public static void LogSuccess(string Name)
+        {
+            string message = string.Format("Time: {0}", DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt"));
+            message += Environment.NewLine;
+            message += "-----------------------------------------------------------";
+            message += Name;
+            //message += Environment.NewLine;
+            //message += string.Format("Message: {0}", ex.Message);
+            //message += Environment.NewLine;
+            //message += string.Format("StackTrace: {0}", ex.StackTrace);
+            //message += Environment.NewLine;
+            //message += string.Format("Source: {0}", ex.Source);
+            //message += Environment.NewLine;
+            //message += string.Format("TargetSite: {0}", ex.TargetSite.ToString());
+            //message += Environment.NewLine;
+            //message += "-----------------------------------------------------------";
+            //message += Environment.NewLine;
+            string path = System.Web.HttpContext.Current.Server.MapPath("~/logs/Log.txt");  
+            using (StreamWriter writer = new StreamWriter(path, true))                      
+            {
+                writer.WriteLine(message);
+                writer.Close();
+            }
+        }
+    }
+}
